8a3c9f89a22ba1e0991f4b52c0ee9c276c349ea8
enoch232 <enoch232@gmail.com>
2016-05-25 18:34:21 +0900

    complete pressing as toggle finish

--- client-side/todo.js
+++ client-side/todo.js
@@ -1,26 +1,51 @@
 function capitalize(string){
 	return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
-function getTodoList(){
-	$.ajax({
-		url: "http://localhost:3000/api/todos", 
-		success: function(todos){
-			console.log(todos);
-			todos.forEach(function(todo){
-				if(todo.finished){
-					$('#todo-holder').append("<p style = 'text-decoration:line-through'>"+capitalize(todo.title)+"</p>");
-				}else{
-					$('#todo-holder').append("<p>"+capitalize(todo.title)+"</p>");
-				}
-			});
-		
-		}
-
-	});
-}
 $(document).ready(function(){
+	
+	function getTodoList(){
+		$.ajax({
+			url: "http://localhost:3000/api/todos", 
+			success: function(todos){
+				console.log(todos);
+				todos.forEach(function(todo){
+					var click;
+					if(todo.finished){
+						var click = $("<p class = 'todo' style = 'text-decoration:line-through' data-finished ='"+todo.finished+"' data-title ='"+todo.title+"' data-id ="+"'"+todo._id+"'"+" >"+capitalize(todo.title)+"</p>");
+					}else{
+						var click = $("<p class = 'todo' data-finished ='"+todo.finished+"' data-title ='"+todo.title+"' data-id ='"+todo._id+"'>"+capitalize(todo.title)+"</p>");
+					}
+					$('#todo-holder').append(click);
+					click.on("click", function(){
+						if (click.data('finished') == false){
+							$.ajax({
+								url: "http://localhost:3000/api/todos/"+click.data('id'), 
+								type: "PUT",
+								data: {
+									title: click.data('title'),
+									finished: true
+								}
+							});
+						}else{
+							$.ajax({
+								url: "http://localhost:3000/api/todos/"+click.data('id'), 
+								type: "PUT",
+								data: {
+									title: click.data('title'),
+									finished: false
+								}
+							});
+						}
+						$('#todo-holder').empty();
+						getTodoList();
+					});
+				});
+			}
+
+		});
+	};
 	//get all data initially.
-	getTodoList();
+	getTodoList()
 	$("#make-btn").on("click",function(){
 		if ($("#title").val().length){
 			$.ajax({
@@ -30,7 +55,7 @@ $(document).ready(function(){
 					'title': $('#title').val()
 				},
 				success: function(data){
-					$('#todo-holder').append("<p>"+capitalize(data.title)+"</p>");
+					$('#todo-holder').append("<p class = 'todo' data-finished ='"+data.finished+"' data-title ='"+data.title+"' data-id ="+"'"+data._id+"'"+" >"+capitalize(data.title)+"</p>");
 				},
 				error: function(){
 					alert("Failed!");
